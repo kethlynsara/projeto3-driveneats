86b76e6d2293ce4dd86409cb0d060a5396e5db40
Kethlyn <kethlynsaraa@gmail.com>
2022-01-31 14:42:12 -0300

    botão de finalizar pedido

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let pratoSelecionado = null;
+let bebidaSelecionada = null;
+let sobremesaSelecionada = null;
+let cont = 0;
+
 function selecionarPrato(classeOpcoes) {
     const selecionado = document.querySelector(".menu-prato .selecionado");
     if (selecionado != null) {
@@ -5,6 +10,9 @@ function selecionarPrato(classeOpcoes) {
     }
     const elemento = document.querySelector(classeOpcoes);
     elemento.classList.add("selecionado");
+
+    pratoSelecionado = classeOpcoes;
+    cont = cont + 1;
 }
 
 function selecionarBebida(classeOpcoes) {
@@ -14,6 +22,9 @@ function selecionarBebida(classeOpcoes) {
     }
     const elemento = document.querySelector(classeOpcoes);
     elemento.classList.add("selecionado");
+
+    bebidaSelecionada = classeOpcoes;
+    cont = cont + 1;
 }
 
 function selecionarSobremesa(classeOpcoes) {
@@ -23,4 +34,43 @@ function selecionarSobremesa(classeOpcoes) {
     }
     const elemento = document.querySelector(classeOpcoes);
     elemento.classList.add("selecionado");
+
+    sobremesaSelecionada = classeOpcoes;
+    cont = cont + 1;
+
+    if (pratoSelecionado !== null && bebidaSelecionada !== null && sobremesaSelecionada !== null) {
+        let pedidoFinal = document.querySelector(".pedido-final");
+        let texto = document.querySelector(".texto-bottom")
+        pedidoFinal.classList.add("pedidosSelecionados");
+        texto.innerHTML = "Fechar pedido";
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
